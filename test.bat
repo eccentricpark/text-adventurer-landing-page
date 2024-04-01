8fdad505a30184f01bdda1334d3ff581f6707df7
Jino <pen40@naver.com>
2024-04-01 23:39:36 +0900

    add .github/workflows

---
+++ test.bat
@@ -0,0 +1,2 @@
+@echo off
+npm run build & cd dist & git init & git add . & git commit -m "first commit" & git remote add origin https://github.com/eccentricpark/eccentricpark.github.io.git & git branch -M main & git push -u origin main
